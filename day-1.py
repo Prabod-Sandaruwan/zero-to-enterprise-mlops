54bce5284130ffb7a0ea19a335503c5fbcfdf6b3
Prabod-Sandaruwan <sandaruwanhapudeniya@gmail.com>
2026-08-10 23:39:52 +0530

    day 1 - wite a program for ask user input and display them on terminal

---
+++ day-1.py
@@ -0,0 +1,11 @@
+# Hello World
+print("Hello World")
+
+# ask name and greet
+name = input("Enter your name: ")
+print("Hello,", name)
+
+# print the first and last names
+first = input("Enter your first name: ")
+last=input("enter your last name : ")
+print(f"{first} {last}")
