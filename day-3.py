74a640db84cc81e6ff0b53aba45f9f908a660565
Prabod-Sandaruwan <sandaruwanhapudeniya@gmail.com>
2026-08-12 22:33:31 +0530

    day 3 added - practice the define a function and return the value. also practice the how to display a return value

---
+++ day-3.py
@@ -0,0 +1,14 @@
+def square(para):
+    return para * para
+
+def rectangle(height ,  width):
+    return height*width
+
+width= int(input('enter the width : '))
+print(square(width))
+
+height=int(input("enter the height : "))
+width=int(input("enter the width : "))
+
+print ('area is : ', rectangle(height,width))
+
